75dfadce9dc4b76c027db0c79f0ed254f841509c
Ezequiel Pereyra <31800576+ezequielPereyra@users.noreply.github.com>
2021-05-07 20:30:01 -0300

    El disparo cambia la gravedad
    Los personajes tienen su propia gravedad. Cuando el personaje cambia la gravedad le avise al nivel que cambe la de todos

--- areaCampoGravedad.gd
+++ areaCampoGravedad.gd
@@ -1,23 +1,17 @@
-extends Node2D
+extends Area2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var Gravedad = preload("res://Gravedad.tscn")
 
-
-var personajes
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	personajes = [$Jugador, $Robotito, $Robotito2]
-	$Jugador.nivel = self
-	
-func invertirGravedadGlobal():
-	for personaje in personajes:
-		personaje.invertirGravedad()
+	pass # Replace with function body.
 
+func chocasteConRobot(robot):
+	robot.invertirGravedad()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
